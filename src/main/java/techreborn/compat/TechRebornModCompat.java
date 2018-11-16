ee5a9c032b5d5ca6c7dc30f866ea44ce110c5a28
modmuss50 <modmuss50@gmail.com>
2018-11-16 16:34:32 +0000

    Sign jar and add a simple mod

---
+++ src/main/java/techreborn/compat/TechRebornModCompat.java
@@ -0,0 +1,7 @@
+package techreborn.compat;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = "techreborn_compat", name = "Tech Reborn Mod Compatibility", version = "1.0.0", certificateFingerprint = "8727a3141c8ec7f173b87aa78b9b9807867c4e6b", dependencies = "required:techreborn")
+public class TechRebornModCompat {
+}
