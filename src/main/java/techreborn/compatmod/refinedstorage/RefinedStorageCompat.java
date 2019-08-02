1c7ccacff0c832fc15cedc67fcb1241808b44eb1
estebes <6414656+estebes@users.noreply.github.com>
2019-08-02 11:09:17 +0100

    Add compatibility with Refined Storage (Processor recipes in the Assembling Machine).

--- src/main/java/techreborn/compatmod/refinedstorage/RefinedStorageCompat.java
+++ src/main/java/techreborn/compatmod/refinedstorage/RefinedStorageCompat.java
@@ -1,9 +1,34 @@
+/*
+ * This file is part of TechReborn, licensed under the MIT License (MIT).
+ *
+ * Copyright (c) 2018 TechReborn
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package techreborn.compatmod.refinedstorage;
 
 import com.raoulvdberge.refinedstorage.RSItems;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import reborncore.common.registration.RebornRegistry;
+import reborncore.common.registration.impl.ConfigRegistry;
 import techreborn.api.recipe.Recipes;
 import techreborn.compat.ICompatModule;
 import techreborn.lib.ModInfo;
@@ -13,15 +38,20 @@ import techreborn.lib.ModInfo;
  */
 @RebornRegistry(modOnly = "refinedstorage", modID = ModInfo.MOD_ID)
 public class RefinedStorageCompat implements ICompatModule {
+	@ConfigRegistry(config = "compat", category = "refinedstorage", key = "EnableAssemblingMachineProcessorRecipes", comment = "Enable assembling machine recipes envolving Refined Storage processors")
+	public static boolean enableAssemblingMachineProcessorRecipes = true;
+
 	@Override
 	public void init(FMLInitializationEvent event) {
+		if (!enableAssemblingMachineProcessorRecipes) return;
+
 		// Basic Processor
 		Recipes.assemblingMachine.createRecipe()
 			.withInput("plateSilicon")
 			.withInput("plateIron")
 			.withOutput(new ItemStack(RSItems.PROCESSOR, 1 , 3))
 			.withEnergyCostPerTick(1)
-			.withEnergyCostPerTick(800)
+			.withOperationDuration(800)
 			.register();
 
 		// Improved Processor
@@ -30,15 +60,16 @@ public class RefinedStorageCompat implements ICompatModule {
 			.withInput("plateGold")
 			.withOutput(new ItemStack(RSItems.PROCESSOR, 1 , 4))
 			.withEnergyCostPerTick(2)
-			.withEnergyCostPerTick(1600)
+			.withOperationDuration(1600)
 			.register();
 
+		// Advanced Processor
 		Recipes.assemblingMachine.createRecipe()
 			.withInput("plateSilicon")
 			.withInput("plateDiamond")
 			.withOutput(new ItemStack(RSItems.PROCESSOR, 1 , 5))
 			.withEnergyCostPerTick(4)
-			.withEnergyCostPerTick(3200)
+			.withOperationDuration(3200)
 			.register();
 	}
 }
